9298824288d35a0f508eb5f9446b212d19fde480
Anup Singh <anup50singh50@gmail.com>
2024-11-03 15:24:57 +0530

    Inital commit

---
+++ src/main/java/com/anup/enotebook/ENoteBookApplication.java
@@ -0,0 +1,13 @@
+package com.anup.enotebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ENoteBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ENoteBookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anup/enotebook/ENoteBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anup.enotebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ENoteBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
